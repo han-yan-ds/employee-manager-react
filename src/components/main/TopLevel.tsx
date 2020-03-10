458272a39aea51a913bc4cc641ea5f6f4214105d
han-yan-ds <hanprogramming@gmail.com>
2020-03-10 16:06:39 -0600

    Added react-router for app and login

---
+++ src/components/main/TopLevel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+const TopLevel = () => (
+  <React.Fragment>
+    <Header/>
+    <Body/>
+  </React.Fragment>
+)
+
+export default TopLevel;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import TopLevel from './components/main/TopLevel';
 import {Provider} from 'react-redux';
 import configureStore from './store';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>, document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={configureStore()}>
+      <TopLevel />
+    </Provider>
+  </BrowserRouter>, document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
+// If you want your TopLevel to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
