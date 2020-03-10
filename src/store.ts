7689992a7c8cf3a7e61d7d38af27c490247d4f37
han-yan-ds <hanprogramming@gmail.com>
2020-03-10 16:17:52 -0600

    Started on initialState

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-
+// const initialState = {
+//   employeeList: 
+// }
 
 function configureStore() {
   return createStore(
     rootReducer, 
+    // initialState,
     applyMiddleware(thunk)
   );
 };
