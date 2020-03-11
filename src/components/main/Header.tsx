640c5e2a5197ef81e02899531f280b01317aeab3
han-yan-ds <hanprogramming@gmail.com>
2020-03-10 19:31:22 -0600

    Finally got logIn and logOut state changes to work

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -10,8 +10,8 @@ function mapStateToProps(st: State) {
 
 function mapDispatchToProps(dispatch: Function) {
   return {
-    handleLogOut: () => dispatch(logOut),
-    handleLogIn: () => dispatch(logIn)
+    handleLogOut: () => dispatch(logOut()),
+    handleLogIn: () => dispatch(logIn())
   }
 }
 
