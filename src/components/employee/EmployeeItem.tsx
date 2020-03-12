611d582c854767e9b1166bf0598e107e66ff2dca
han-yan-ds <hanprogramming@gmail.com>
2020-03-12 15:12:18 -0600

    Added labels to ProfileForm

--- src/components/employee/EmployeeItem.tsx
+++ src/components/employee/EmployeeItem.tsx
@@ -6,11 +6,11 @@ const EmployeeItem = (props: {
 }) => (
   <li>
     <span>{`${props.employee.nameString} ${props.employee.isActive}`}</span>
-
+    {/* active/inactive button */}
     <button onClick={() => props.handleChangeStatus()}>
       Update Active/Inactive
     </button>
-
+    {/* edit profile button */}
     <button onClick={() => props.handleShowProfileForm()}>Profile</button>
   </li>
 )
--- src/components/employee/EmployeeProfileForm.tsx
+++ src/components/employee/EmployeeProfileForm.tsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import Employee from '../../types/Employee';
 import {convertDateToHtmlInput} from '../../util/util';
 import {hideProfileModal} from '../../actions/actions';
+import '../../styles/general.scss';
 
 function mapDispatchToProps(dispatch: Function) {
   return {
@@ -18,13 +19,46 @@ const EmployeeProfileForm = (
 
   return <div className='modal'>
     <form>
-      <input type='text' name='firstName'  placeholder='First Name' value={name.fName}/>
-      <input type='text' name='middleInitial' value={(name.mInitial) ? name.mInitial : ''}/>
-      <input type='text' name='lastName' placeholder='Last Name' value={name.lName}/>
-      <input type='date' name='dateOfBirth' value={convertDateToHtmlInput(dateOfBirth)}/>
-      <input type='date' name='dateOfEmployment' value={convertDateToHtmlInput(dateOfEmployment)}/>
+      <label>First Name<span className='required-asterisk'>*</span></label>
+      <input 
+        type='text' 
+        name='firstName' 
+        required={true}
+        placeholder={name.fName}
+      />
+      <label>Middle Initial</label>
+      <input 
+        type='text' 
+        name='middleInitial' 
+        placeholder={(name.mInitial) ? name.mInitial : ''}
+        size={1}
+      />
+      <label>Last Name<span className='required-asterisk'>*</span></label>
+      <input 
+        type='text' 
+        name='lastName'
+        required={true}
+        placeholder={name.lName}
+      />
+      <label>Date Of Birth<span className='required-asterisk'>*</span></label>
+      <input 
+        type='date' 
+        name='dateOfBirth'
+        required={true}
+        placeholder={convertDateToHtmlInput(dateOfBirth)}
+      />
+      <label>Date Of Employment<span className='required-asterisk'>*</span></label>
+      <input 
+        type='date' 
+        name='dateOfEmployment'
+        required={true} 
+        placeholder={convertDateToHtmlInput(dateOfEmployment)}
+      />
     </form>
+    {/* cancel button */}
     <button onClick={() => cancelModal()}>X</button>
+    {/* save button */}
+    <button>Save Changes</button>
   </div>
 
 }
