30e8d327039e3276ad3231cab21bcbff25f59639
han-yan-ds <hanprogramming@gmail.com>
2020-03-12 09:15:30 -0600

    Added feature to changeEmployeeStatus to be optionally take in the desired status as an arg

--- src/util/changeEmployeeList.ts
+++ src/util/changeEmployeeList.ts
@@ -1,9 +1,17 @@
 import Employee from '../types/Employee';
 
-export const changeActiveStatus = (employeeList: Employee[], employeeId: string): Employee[] => {
+export const changeActiveStatus = (
+    employeeList: Employee[], 
+    employeeId: string, 
+    newStatus: boolean | undefined = undefined
+  ): Employee[] => {
   const employeeListClone = [...employeeList];
   const targetEmployee: Employee = employeeListClone.find((employee) => employee.id === employeeId)!;
-  targetEmployee.toggleActive();
-  console.log(targetEmployee.isActive);
+  if (newStatus === undefined) {
+    targetEmployee.toggleActive();
+  } else {
+    targetEmployee.isActive = newStatus;
+  }
+  // console.log(targetEmployee.isActive);
   return employeeListClone;
 }
