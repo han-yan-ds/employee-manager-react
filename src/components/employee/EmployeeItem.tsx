81db3f5fd338310427819ee01be3ef0d1688ecc1
Han Yan <hanprogramming@gmail.com>
2020-03-13 08:58:43 -0600

    Refactored employeelist to BootstrapTable

--- src/components/employee/EmployeeItem.tsx
+++ src/components/employee/EmployeeItem.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Employee from '../../types/Employee';
+import {ListGroup} from 'react-bootstrap';
 
 const EmployeeItem = (props: {
   employee: Employee, handleChangeStatus: Function, handleShowProfileForm: Function
   }) => (
-  <li>
-    <span>{`${props.employee.nameString} ${props.employee.isActive}`}</span>
+  <tr>
+    <td>{props.employee.nameString}</td>
+    <td>{props.employee.dateOfEmploymentString}</td>
+    <td>{props.employee.dateOfBirthString}</td>
+    <td>{(props.employee.isActive) ? "Active" : "Inactive"}</td>
     {/* active/inactive button */}
     <button onClick={() => props.handleChangeStatus()}>
       Update Active/Inactive
     </button>
     {/* edit profile button */}
     <button onClick={() => props.handleShowProfileForm()}>Profile</button>
-  </li>
+  </tr>
 )
 
 export default EmployeeItem;
