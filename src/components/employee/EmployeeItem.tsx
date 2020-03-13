e1d739d9c630a827cc785de71aa73ab93be6b1a6
Han Yan <hanprogramming@gmail.com>
2020-03-13 09:25:50 -0600

    Bootstrap-ized the edit profile buttons

--- src/components/employee/EmployeeItem.tsx
+++ src/components/employee/EmployeeItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Employee from '../../types/Employee';
-import {ListGroup} from 'react-bootstrap';
+import {Dropdown, DropdownButton} from 'react-bootstrap';
 
 const EmployeeItem = (props: {
   employee: Employee, handleChangeStatus: Function, handleShowProfileForm: Function
@@ -9,13 +9,16 @@ const EmployeeItem = (props: {
     <td>{props.employee.nameString}</td>
     <td>{props.employee.dateOfEmploymentString}</td>
     <td>{props.employee.dateOfBirthString}</td>
-    <td>{(props.employee.isActive) ? "Active" : "Inactive"}</td>
-    {/* active/inactive button */}
-    <button onClick={() => props.handleChangeStatus()}>
-      Update Active/Inactive
-    </button>
-    {/* edit profile button */}
-    <button onClick={() => props.handleShowProfileForm()}>Profile</button>
+    <td>{(props.employee.isActive) ? "Active" : "Inactive"}
+      <DropdownButton id={`options-${props.employee.id}`} title='' variant='Secondary' className="inline">
+        <Dropdown.Item onClick={() => props.handleChangeStatus()}>
+          {`Set ${(props.employee.isActive) ? "Inactive" : "Active"}`}
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => props.handleShowProfileForm()}>
+          Edit Profile
+        </Dropdown.Item>
+      </DropdownButton>
+    </td>
   </tr>
 )
 
