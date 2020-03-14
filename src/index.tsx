530f7d1556f299e43fcb43fcdf282cfba391cdc6
Han Yan <hanprogramming@gmail.com>
2020-03-14 17:47:17 -0600

    Cleaning on old Router code that should no longer be here

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import Router from './components/Router';
 import {Provider} from 'react-redux';
 import configureStore from './store';
-import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
     <Provider store={configureStore()}>
       <Router />
-    </Provider>
-  </BrowserRouter>, document.getElementById('root')
+    </Provider>, document.getElementById('root')
 );
 
 // If you want your TopLevel to work offline and load faster, you can change
