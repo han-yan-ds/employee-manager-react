cf3ab148a498f8f37c8fd321800c5af08f081a52
Han Yan <hanprogramming@gmail.com>
2020-03-14 08:47:31 -0600

    Rendered the login

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Modal, Form, Button} from 'react-bootstrap';
+
+
 
 const Login = () => (
-  <div>
-    Please Log In!
-  </div>
+  <Modal.Dialog>
+    <Modal.Header>
+      <Modal.Title>Please Log In!</Modal.Title>
+    </Modal.Header>
+    <Form>
+      <Form.Group controlId='user-name'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control required/>
+      </Form.Group>
+        <Form.Label>Password</Form.Label>
+        <Form.Control required type="password" />
+    </Form>
+    <Modal.Footer>
+      <Button variant='primary' onClick={() => console.log('clicked')}>Login</Button>
+    </Modal.Footer>
+  </Modal.Dialog>
 )
 
 export default Login;
