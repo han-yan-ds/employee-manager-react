00cc6c441bfd5c0fc63bfa42499c700d67d1efe6
Han Yan <hanprogramming@gmail.com>
2020-03-14 11:22:27 -0600

    Got Login and Logout to work

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {logIn} from '../../actions/actions';
+import {logIn, showLoginFailure} from '../../actions/actions';
 import {Modal, Form, Button} from 'react-bootstrap';
 import {getInputValueById, isValidCredentials} from '../../util/util';
 import crypto from 'crypto';
 import { ClickHandler } from '../../types/types';
-const shasum = crypto.createHash('sha256');
 
-// const handleFailedLogin() {
-
-// }
-
-const loginHandler = async (e: React.FormEvent, onSuccess: Function) => {
+const loginHandler = async (e: React.FormEvent, onSuccess: Function, onFailure: Function) => {
   e.preventDefault();
+  const shasum = crypto.createHash('sha256');
   const username: string = getInputValueById('user-name');
   const hash: string = shasum.update(getInputValueById('pass-word')).digest('hex');
-  if (await isValidCredentials(username, hash)) {
-    onSuccess();
-  } else {
-    //onFailure();
-  }
+  (await isValidCredentials(username, hash)) ? onSuccess() : onFailure();
+}
+
+function mapStateToProps({showLoginFailure}: {showLoginFailure: boolean}) {
+  return {showLoginFailure};
 }
 
 function mapDispatchToProps(dispatch: Function) {
   return {
-    handleLogin: () => dispatch(logIn()),
+    handleLogin: () => {
+      dispatch(logIn());
+      dispatch(showLoginFailure(false));
+    },
+    handleFailedLogin: () => dispatch(showLoginFailure(true))
   }
 }
 
-const Login = ({handleLogin}: {handleLogin: ClickHandler}) => (
-  <Modal.Dialog>
+const Login = ({showLoginFailure, handleLogin, handleFailedLogin}: 
+  {showLoginFailure: boolean, handleLogin: ClickHandler, handleFailedLogin: Function}) => {
+
+  const showLoginFeedback = () => (showLoginFailure) ? <div className='error'>Login Failed, Please Try Again</div> : null;
+
+  return <Modal.Dialog>
     <Modal.Header>
       <Modal.Title>Please Log In!</Modal.Title>
     </Modal.Header>
@@ -42,11 +46,12 @@ const Login = ({handleLogin}: {handleLogin: ClickHandler}) => (
         <Form.Label>Password</Form.Label>
         <Form.Control required type="password" defaultValue=''/>
       </Form.Group>
+      {showLoginFeedback()}
     </Form>
     <Modal.Footer>
-      <Button variant='primary' onClick={(e: React.MouseEvent) => loginHandler(e, handleLogin)}>Login</Button>
+      <Button variant='primary' onClick={(e: React.MouseEvent) => loginHandler(e, handleLogin, handleFailedLogin)}>Login</Button>
     </Modal.Footer>
   </Modal.Dialog>
-)
+}
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
